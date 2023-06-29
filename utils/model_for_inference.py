5a3402c577f3df2976d760f5456de3525bb6e6cb
doh0106 <121469539+doh0106@users.noreply.github.com>
2023-06-29 14:28:20 +0900

    Update model_for_inference.py

--- utils/model_for_inference.py
+++ utils/model_for_inference.py
@@ -1,25 +1,32 @@
 import sys 
 sys.path.append("./")
 
+from transformers import RobertaTokenizerFast, RobertaModel, RobertaConfig
 from transformers import BertModel, BertConfig,  BertTokenizerFast
 from src.transform import SelectionJoinTransform
 from src.encoder import PolyEncoder, CrossEncoder
 import torch
 import os
 
-def Load_Model_Tokenizer(model_path, model_type='poly'):
+def Load_Model_Tokenizer(model_path, model_type='poly', bert_type='bert'):
 
-    bert_config = BertConfig.from_json_file(os.path.join(model_path, 'config.json'))
+    MODEL_CLASSES = {
+        'bert': (BertConfig, BertTokenizerFast, BertModel),
+        'roberta' : (RobertaConfig, RobertaTokenizerFast, RobertaModel)
+    }
+    ConfigClass, TokenizerClass, BertModelClass = MODEL_CLASSES[bert_type]
+
+    bert_config = ConfigClass.from_json_file(os.path.join(model_path, 'config.json'))
     previous_model_file = os.path.join(model_path, 'pytorch_model.bin')
     print(previous_model_file)
 
     model_state_dict = torch.load(previous_model_file, map_location="cpu")
 
-    tokenizer = BertTokenizerFast.from_pretrained(model_path, do_lower_case=True, clean_text=False)
+    tokenizer = TokenizerClass.from_pretrained(model_path, do_lower_case=True, clean_text=False)
     tokenizer.add_tokens(['\n'], special_tokens=True)
     context_transform = SelectionJoinTransform(tokenizer=tokenizer, max_len=128)
 
-    bert = BertModel(bert_config)
+    bert = BertModelClass(bert_config)
     bert.resize_token_embeddings(len(tokenizer))
     if model_type=='poly': 
         model = PolyEncoder(bert_config, bert=bert, poly_m=16)
